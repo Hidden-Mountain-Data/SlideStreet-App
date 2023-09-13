7451e6f12d0e7d45e5cbf6ecf7b489142c2cab62
Wade Pate <wadejp8@gmail.com>
2023-09-13 12:52:02 -0600

    seperated types; setup axiosSetup file;

--- server/controllers/iotController.ts
+++ server/controllers/iotController.ts
@@ -1,66 +1,17 @@
-import axios, { AxiosError } from 'axios';
-import { IOT_ENDPOINT } from '../config';
 import {
   CLIENT_ID_KEY,
   CLIENT_SECRET_KEY,
   GRANT_TYPE,
   PASSWORD_TYPE,
 } from '../constants';
-import axiosInstance from '../setup/axiosSetup';
-import { DevicesResponse } from '../types/iot/iotDevices';
-
-interface AccessTokenParamsPassword {
-  grantType: string;
-  username: string;
-  password: string;
-  passwordType: string;
-  clientId: string;
-  clientSecret: string;
-}
-
-interface AccessTokenParamsRefresh {
-  grantType: string;
-  clientId: string;
-  clientSecret: string;
-  refreshToken: string;
-}
-
-interface GatewayClient {
-  iface: string;
-  mac: string;
-  ipAddress: string;
-  hostname: string;
-  ttl: number;
-}
-
-interface GatewayClientsResult {
-  oid: string;
-  count: number;
-  clients: GatewayClient[];
-  updatedAt: string;
-  _id: string;
-}
-
-interface GatewayClientsResponse {
-  result: GatewayClientsResult;
-}
-
-interface OAuth2Token {
-  access_token: string;
-  expires_in: number;
-  refresh_token: string;
-  token_type: string;
-}
-
-interface CustomError extends Error {
-  data?: any;
-}
-
-const handleAxiosError = (error: AxiosError): CustomError => {
-  const customError = new Error(error.message) as CustomError;
-  customError.data = error.response?.data;
-  return customError;
-};
+import { axiosCall } from '../setup/axiosSetup';
+import {
+  AccessTokenParamsPassword,
+  AccessTokenParamsRefresh,
+  DevicesResponse,
+  GatewayClientsResponse,
+  OAuth2Token,
+} from '../types/iot/iotDevices';
 
 export const getAccessTokenWithPassword = async ({
   grantType,
@@ -70,7 +21,7 @@ export const getAccessTokenWithPassword = async ({
   clientId,
   clientSecret,
 }: AccessTokenParamsPassword): Promise<OAuth2Token> => {
-  const url = `${IOT_ENDPOINT}/oauth2/access_token`;
+  const url = `/oauth2/access_token`;
   const postData: Record<string, string> = {
     [GRANT_TYPE]: grantType,
     username,
@@ -82,61 +33,35 @@ export const getAccessTokenWithPassword = async ({
 
   const formData = new URLSearchParams(postData).toString();
 
-  try {
-    const response = await axiosInstance.post('/oauth2/access_token', formData);
-    return response.data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      throw handleAxiosError(error);
-    } else {
-      throw new Error('An unknown error occurred');
-    }
-  }
+  return await axiosCall<OAuth2Token>('post', url, formData);
 };
 
 export const getDeviceGatewayClients = async (
   accessToken: string,
   deviceId: string,
 ): Promise<GatewayClientsResponse> => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+  const url = `/api/devices/${deviceId}/gateway/clients`;
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
   };
 
-  try {
-    const response = await axiosInstance.get<GatewayClientsResponse>(
-      `/api/devices/${deviceId}/gateway/clients`,
-      config,
-    );
-    return response.data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      console.error('Something went wrong:', error.response?.data);
-    }
-    throw new Error('Failed to fetch gateway clients');
-  }
+  return await axiosCall<GatewayClientsResponse>(
+    'get',
+    url,
+    undefined,
+    headers,
+  );
 };
 
 export const getDevices = async (
   accessToken: string,
 ): Promise<DevicesResponse> => {
-  const url = `${IOT_ENDPOINT}/api/devices`;
-  const config = {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+  const url = `/api/devices`;
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
   };
-  try {
-    const response = await axiosInstance.get<DevicesResponse>(url, config);
-    return response.data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      throw handleAxiosError(error);
-    } else {
-      throw new Error('An unknown error occurred');
-    }
-  }
+
+  return await axiosCall<DevicesResponse>('get', url, undefined, headers);
 };
 
 export const refreshAccessToken = async ({
@@ -152,17 +77,11 @@ export const refreshAccessToken = async ({
     refresh_token: refreshToken,
   };
 
-  try {
-    const response = await axiosInstance.post(
-      '/oauth2/access_token',
-      new URLSearchParams(postData).toString(),
-    );
-    return response.data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      throw handleAxiosError(error);
-    } else {
-      throw new Error('An unknown error occurred');
-    }
-  }
+  const payload = new URLSearchParams(postData).toString();
+
+  return await axiosCall<OAuth2Token | null>(
+    'post',
+    '/oauth2/access_token',
+    payload,
+  );
 };
--- server/setup/axiosSetup.ts
+++ server/setup/axiosSetup.ts
@@ -1,12 +1,36 @@
-import axios from 'axios';
-import { IOT_ENDPOINT } from '../config';
+import axios, { AxiosError } from 'axios';
+import { CONNECTION_STRING, IOT_ENDPOINT } from '../config';
 import { CONTENT_TYPE } from '../constants';
+import CustomError from '../utils/CustomError';
 
 const axiosInstance = axios.create({
-  baseURL: IOT_ENDPOINT,
+  baseURL:
+    process.env.NODE_ENV === 'development' ? CONNECTION_STRING : IOT_ENDPOINT,
   headers: {
     'Content-Type': CONTENT_TYPE,
   },
 });
+
+export const axiosCall = async <T>(
+  method: 'get' | 'post',
+  url: string,
+  data?: any,
+  headers?: any,
+): Promise<T> => {
+  try {
+    const response = await axiosInstance[method]<T>(url, data, { headers });
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw handleAxiosError(error);
+    }
+    console.error('Unknown error:', error);
+    throw new Error('An unknown error occurred');
+  }
+};
+
+const handleAxiosError = (error: AxiosError): CustomError => {
+  return new CustomError(error.message, error.response?.data);
+};
 
 export default axiosInstance;

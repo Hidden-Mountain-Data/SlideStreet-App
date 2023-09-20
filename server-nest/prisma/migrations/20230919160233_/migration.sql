09b63c6ee9dc333e8f425863b2454a95c84b064a
Wade Pate <wadejp8@gmail.com>
2023-09-20 12:36:57 -0600

    cleaned up file structure

--- server-nest/prisma/migrations/20230919160233_/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "imageUrl" VARCHAR(250),
-ADD COLUMN     "imageUuid" VARCHAR(36),
-ADD COLUMN     "is_active" BOOLEAN NOT NULL DEFAULT false;
--- server-nest/src/modules/users/dto/address.dto.ts
+++
@@ -1,7 +0,0 @@
-// * Commented out in order to push
-// TODO: WIP
-// import { IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
-// import { Expose, classToPlain, Transform } from 'class-transformer';
-// import { Role } from '../../rbac/role.enum';
-
-export class AddressDto {}

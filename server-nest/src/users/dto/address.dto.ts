e1078535334e1709f3cf7ba0943128cfdb3f937c
nickknezevich <48396463+nickknezevich@users.noreply.github.com>
2023-09-20 07:20:49 -0700

    Address dto add

--- server-nest/src/users/dto/address.dto.ts
+++ server-nest/src/users/dto/address.dto.ts
@@ -4,37 +4,4 @@ import { Role } from '../../rbac/role.enum';
 
 export class AddressDto {
   
-  @IsNotEmpty()
-  @IsEmail()
-  
-  email: string;
-
-  @IsNotEmpty()
-  @Expose({ name: 'first_name' })
-  firstName: string;
-
-  @IsNotEmpty()
-  @Expose({ name: 'last_name' })
-  lastName: string;
-
-  @IsOptional()
-  @Expose({ name: 'full_name' })
-  fullName: string;
-
-  role: Role;
-
-  @IsNotEmpty()
-  password: string;
-
-  @IsOptional()
-  phone: string;
-
-  @IsOptional()
-  @Expose({ name: 'stripe_customer_id' })
-  stripeCustomerId: string;
-
-  constructor(partial: Partial<CreateUserDto> = {}) {
-    Object.assign(this, partial);
-  }
-
 }
